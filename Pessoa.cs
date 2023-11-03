e9c2b89901b3fc8192579add27ef2762af1ec319
Vilberto Junior <149752352+Vilbj1@users.noreply.github.com>
2023-11-03 01:24:30 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FazendoIMC
+{
+    public class Pessoa
+    {
+        //criação dos atributos
+        public required string nome;
+
+        public double peso;
+
+        public double altura;
+
+        //Métodos
+
+        public double imc()
+        {
+            return peso/(altura * altura);
+        }
+
+        public string Situation(double imc)
+        {
+            if (imc < 18.5)
+            {
+                return "Abaixo do peso";
+            }
+
+            if (imc < 25)
+            {
+                return "Peso normal";
+            }
+            
+            if (imc < 30)
+            {
+                return "Acima do peso";
+            }
+            return "Obeso";
+        }
+
+        //mensagem
+
+        public void resultado()
+        {
+            double obterImc = imc();
+
+            string obterSituacao = Situation(obterImc);
+
+            Console.WriteLine(nome + " está com o imc de " + obterImc + " e está " + obterSituacao);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FazendoIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciar objeto para o imc
+            Pessoa a = new()
+            {
+                nome = "Vilberto",
+                peso = 90,
+                altura = 1.63,
+            };
+            a.resultado();
+        }
+    }
+}
